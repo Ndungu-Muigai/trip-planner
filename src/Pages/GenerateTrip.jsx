5eea660c3116c9d635a1956fe42bd93f3dc59195
Ndungu-Muigai <ndungu.muigai01@gmail.com>
2025-09-15 17:02:47 +0300

    Added functionality to show that a trip is being generated. Alos made the pickup and dropoff location inputs responsive on smaller devices

--- src/Pages/GenerateTrip.jsx
+++ src/Pages/GenerateTrip.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-
+import "leaflet/dist/leaflet.css"
 import { toast } from "react-toastify"
 import { useNavigate } from "react-router-dom"
 import { CircularProgress } from "@mui/material"
@@ -9,82 +9,82 @@ import { CircularProgress } from "@mui/material"
 const GenerateTrip = () => 
 {
     const navigate = useNavigate()
-    const BACKEND_URL=import.meta.env.VITE_BACKEND_URL
+    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
-    console.log(BACKEND_URL)
-    //Setting the query parameters and the results
+    // Queries and results
     const [pickupQuery, setPickupQuery] = useState("")
     const [dropoffQuery, setDropoffQuery] = useState("")
     const [pickupResults, setPickupResults] = useState([])
     const [dropoffResults, setDropoffResults] = useState([])
 
+    // Selected values
     const [selectedPickup, setSelectedPickup] = useState(null)
     const [selectedDropoff, setSelectedDropoff] = useState(null)
 
-    // Prevent double fetch after selecting
+    // Prevent double fetch
     const [pickupSelected, setPickupSelected] = useState(false)
     const [dropoffSelected, setDropoffSelected] = useState(false)
 
-    //States to track if the search results have been fetched
+    // Loading states
     const [pickupLoading, setPickupLoading] = useState(false)
     const [dropoffLoading, setDropoffLoading] = useState(false)
 
     const [currentLocation, setCurrentLocation] = useState(null)
     const [cycle, setCycle] = useState()
 
-    //Fetching the current location
-    useEffect(()=>
-    {
-        if(navigator.geolocation)
-        {
-            navigator.geolocation.getCurrentPosition(position => setCurrentLocation([position.coords.latitude, position.coords.longitude]))
-        }
-    },[])
+    const [generatingTrip, setGeneratingTrip] = useState(false)
 
-    //Searching pickup locations based on user input
-    useEffect(()=>
+    // Fetch current location
+    useEffect(() => 
     {
-        if(pickupQuery.length > 2 && !pickupSelected)
+        if (navigator.geolocation) 
+        {
+            navigator.geolocation.getCurrentPosition(position => setCurrentLocation([position.coords.latitude, position.coords.longitude,]))
+        }
+    }, [])
+
+    // Pickup search
+    useEffect(() => 
+    {
+        if (pickupQuery.length > 2 && !pickupSelected) 
         {
             setPickupLoading(true)
             const timeOut = setTimeout(() => 
             {
                 fetch(`${BACKEND_URL}/api/locations/search?text=${pickupQuery}`)
-                .then(response => response.json())
-                .then(data => setPickupResults(data.features || []))
-                .catch(err => toast.error(err))
-                .finally(()=> setPickupLoading(false))
+                .then((response) => response.json())
+                .then((data) => setPickupResults(data.features || []))
+                .catch((err) => toast.error(err))
+                .finally(() => setPickupLoading(false))
             }, 500)
 
             return () => clearTimeout(timeOut)
         }
+    }, [pickupQuery, pickupSelected])
 
-    },[pickupQuery, pickupSelected])
-
-    //Searching dropoff locations based on user input
-    useEffect(()=>
+    // Dropoff search
+    useEffect(() => 
     {
-        if(dropoffQuery.length > 2 && !dropoffSelected)
+        if (dropoffQuery.length > 2 && !dropoffSelected) 
         {
             setDropoffLoading(true)
             const timeOut = setTimeout(() => 
             {
                 fetch(`${BACKEND_URL}/api/locations/search?text=${dropoffQuery}`)
-                .then(response => response.json())
-                .then(data => setDropoffResults(data.features || []))
-                .catch(err => toast.error(err))   
-                .finally(()=> setDropoffLoading(false))
+                .then((response) => response.json())
+                .then((data) => setDropoffResults(data.features || []))
+                .catch((err) => toast.error(err))
+                .finally(() => setDropoffLoading(false))
             }, 500)
 
             return () => clearTimeout(timeOut)
         }
+    }, [dropoffQuery, dropoffSelected])
 
-    },[dropoffQuery, dropoffSelected])
-
-    const generateTrip = async e =>
+    const generateTrip = async e => 
     {
         e.preventDefault()
-        if(!selectedPickup || !selectedDropoff || !cycle)
+        if (!selectedPickup || !selectedDropoff || !cycle) 
         {
             toast.error("Please fill out all the fields")
             return
@@ -92,126 +92,130 @@ const GenerateTrip = () =>
 
         const body = 
         {
-            "current_location": currentLocation,
-            "pickup_location": [selectedPickup[1], selectedPickup[0]],
-            "dropoff_location": [selectedDropoff[1], selectedDropoff[0]],
-            "cycle_used": cycle
+            current_location: currentLocation,
+            pickup_location: [selectedPickup[1], selectedPickup[0]],
+            dropoff_location: [selectedDropoff[1], selectedDropoff[0]],
+            cycle_used: cycle,
         }
 
-        console.log(body)
-        
-        try
+        try 
         {
-            const response = await fetch(`${BACKEND_URL}/api/trips/`,
+            setGeneratingTrip(true)
+            const response = await fetch(`${BACKEND_URL}/api/trips/`, 
             {
                 method: "POST",
                 headers: 
                 {
                     "Content-Type": "application/json",
-                    "Accept": "application/json"
+                    Accept: "application/json",
                 },
-                body: JSON.stringify(body)
+                body: JSON.stringify(body),
             })
 
-            if(!response.ok)
+            if (!response.ok) 
             {
                 const error = await response.json()
                 toast.error(error)
                 return
             }
 
-            const data = await(response.json())
+            const data = await response.json()
             navigate(`/trips/${data.id}`)
-            toast.success(`Trip created successfully with ID: ${data.id}`)
-        }
+            toast.success("Trip created successfully")
+            setGeneratingTrip(false)
+        } 
         catch (error) 
         {
             toast.error(`Error creating trip: ${error}`)
         }
     }
 
-    return ( 
+    return (
         <div className="pt-3 w-4/5 mx-auto">
             {
-                currentLocation && 
-                <MapContainer center={currentLocation} zoom={13} className="h-56 rounded-lg shadow z-10">
-                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy OpenStreetMap contributors"/>
-                    <Marker position={currentLocation}>
-                        <Popup>You are here</Popup>
-                    </Marker>
-                </MapContainer>
+                currentLocation && (
+                    <MapContainer center={currentLocation} zoom={13} className="h-56 rounded-lg shadow z-10">
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy OpenStreetMap contributors"/>
+                        <Marker position={currentLocation}>
+                            <Popup>You are here</Popup>
+                        </Marker>
+                    </MapContainer>
+                )
             }
+
             <form onSubmit={generateTrip} className="mt-9">
                 <div className="flex flex-col justify-center md:flex-row gap-6">
+                    {/* Pickup Input */}
                     <div className="relative w-full">
-                        <input type="search" className="input bg-inherit border border-black " required placeholder="Enter pick up location" value={pickupQuery} onChange={e => 
-                            {
-                                setPickupQuery(e.target.value)
-                                setPickupSelected(false)
-                            }
-                        }/>
+                        <input type="search" className="input bg-inherit border border-black w-full" required placeholder="Enter pick up location" disabled={generatingTrip} value={pickupQuery}
+                        onChange={(e) => 
                         {
-                            pickupLoading &&
+                            setPickupQuery(e.target.value)
+                            setPickupSelected(false)
+                        }}/>
+                        {
+                            pickupLoading && 
+                            (
                                 <div className="absolute z-10 bg-white border border-gray-300 w-full p-2 text-gray-500 flex justify-center items-center">
-                                    <CircularProgress size={22}/>
+                                    <CircularProgress size={22} />
                                 </div>
+                            )
                         }
                         {
-                            pickupResults.length > 0 &&
-                            <ul className="absolute z-10 bg-white border border-gray-300 w-full max-h-40 overflow-y-auto">
-                                {
-                                    pickupResults.map((item, idx) =>
+                            pickupResults.length > 0 && 
+                            (
+                                <ul className={`bg-white border border-gray-300 w-full max-h-40 overflow-y-auto ${pickupResults.length > 0 ? "mt-1" : ""} md:absolute md:mt-0 md:z-10`}>
                                     {
-                                        return(
-                                            <li key={idx} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={()=>
+                                        pickupResults.map((item, idx) => (
+                                        <li key={idx} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={() => 
                                             {
-                                                setPickupQuery(item.properties.label)
-                                                setSelectedPickup(item.geometry.coordinates)
-                                                setPickupResults([])
-                                                setPickupSelected(true)
-                                            }
-                                            }>{item.properties.label}</li>
-                                        )
-                                    })
-                                }
-                            </ul>
+                                            setPickupQuery(item.properties.label)
+                                            setSelectedPickup(item.geometry.coordinates)
+                                            setPickupResults([])
+                                            setPickupSelected(true)
+                                            }}>{item.properties.label}</li>
+                                        ))
+                                    }
+                                </ul>
+                            )
                         }
                     </div>
+
+                    {/* Dropoff Input */}
                     <div className="relative w-full">
-                        <input type="search" className="input bg-inherit border border-black " required placeholder="Enter drop off location" value={dropoffQuery} onChange={e => 
-                            {
-                                setDropoffQuery(e.target.value)
-                                setDropoffSelected(false)
-                            }
-                        }/>
+                        <input type="search" className="input bg-inherit border border-black w-full" required disabled={generatingTrip} placeholder="Enter drop off location" value={dropoffQuery}
+                        onChange={e => 
                         {
-                            dropoffLoading &&
-                                <div className="absolute z-10 bg-white border border-gray-300 w-full p-2 text-gray-500 flex justify-center items-center">
-                                    <CircularProgress size={22}/>
-                                </div>
+                            setDropoffQuery(e.target.value)
+                            setDropoffSelected(false)
+                        }}/>
+                        {
+                            dropoffLoading && 
+                            (
+                            <div className="absolute z-10 bg-white border border-gray-300 w-full p-2 text-gray-500 flex justify-center items-center">
+                                <CircularProgress size={22} />
+                            </div>
+                            )
                         }
                         {
-                            dropoffResults.length > 0 &&
-                            <ul className="absolute z-10 bg-white border border-gray-300 w-full max-h-40 overflow-y-auto">
-                                {
-                                    dropoffResults.map((item, idx) =>
+                            dropoffResults.length > 0 && (
+                            <ul className={` bg-white border border-gray-300 w-full max-h-40 overflow-y-auto ${dropoffResults.length > 0 ? "mt-1" : ""} md:absolute md:mt-0 md:z-10`}>
+                                {dropoffResults.map((item, idx) => (
+                                <li key={idx} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={() => 
                                     {
-                                        return(
-                                            <li key={idx} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={()=>
-                                            {
-                                                setDropoffQuery(item.properties.label)
-                                                setSelectedDropoff(item.geometry.coordinates)
-                                                setDropoffResults([])
-                                                setDropoffSelected(true)
-                                            }
-                                            }>{item.properties.label}</li>
-                                        )
-                                    })
-                                }
+                                        setDropoffQuery(item.properties.label)
+                                        setSelectedDropoff(item.geometry.coordinates)
+                                        setDropoffResults([])
+                                        setDropoffSelected(true)
+                                    }}>{item.properties.label}</li>
+                                ))}
                             </ul>
+                            )
                         }
                     </div>
-                    <select defaultValue="Select your current cycle" className="select bg-inherit border-black w-full z-50" value={cycle} onChange={e => setCycle(e.target.value)}>
+
+                    {/* Cycle Select */}
+                    <select defaultValue="Select your current cycle" className="select bg-inherit border-black w-full z-50" value={cycle} onChange={(e) => setCycle(e.target.value)} disabled={generatingTrip}>
                         <option disabled={true}>Select your current cycle</option>
                         <option value={11}>11 hour cycle</option>
                         <option value={14}>14 hour cycle</option>
@@ -219,11 +223,19 @@ const GenerateTrip = () =>
                 </div>
 
                 <div className="flex justify-center mt-5">
-                    <button className="btn btn-success text-white" type="submit">Generate new trip</button>
+                    <button className="btn btn-success text-white" type="submit" disabled={generatingTrip}>
+                        {
+                            generatingTrip
+                            ?
+                                <CircularProgress size={22}/>
+                            :
+                                "Generate new trip"
+                        }
+                    </button>
                 </div>
             </form>
         </div>
-     )
+    )
 }
- 
+
 export default GenerateTrip
